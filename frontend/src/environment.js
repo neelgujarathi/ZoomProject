566a3e75810cb0e4f7f7e9aa2004458cc60ed31d
Neel Gujarathi <neelgujarathi66@gmail.com>
2025-11-21 19:04:13 +0530

    Add environment.js with backend URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,7 @@
-let IS_PROD = false;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-    "http://localhost:8000"
+let IS_PROD = true;  // true for Render deployment
+
+const server = IS_PROD
+  ? "https://zoomproject-1.onrender.com"  // 👈 replace with your backend Render URL
+  : "http://localhost:8000";
 
 export default server;
